b677c551bdd2ba6f5102b288ff2206826d2e2911
freda suyi <pseuyi@gmail.com>
2022-06-10 16:13:35 -0400

    big bio update

--- src/Bio.svelte
+++ src/Bio.svelte
@@ -5,16 +5,19 @@
     interested in writing elegant, functional, and safe software.
   </header>
   <br/>
-  <header>currently</header>
+  <header><b>currently</b></header>
   <p>
-  they work on <a href="https://www.grailed.com/trust/authentication">digital authentication tools</a> at <a href="https://www.grailed.com/">grailed.com</a>.
+  they work on a generative text tool powering the <a href="https://www.costarastrology.com/">co-star app</a> using functional programming paradigms.
   </p>
-  <header>recently</header>
+  <header><b>in the past</b></header>
+  <p>
+  they worked on <a href="https://www.grailed.com/trust/authentication">digitally authenticating luxury items</a> at <a href="https://www.grailed.com/">grailed.com</a>.
+  </p>
   <p>
   they built a <a href="https://youtu.be/dWbsa3GcDNA">tax dashboard</a> for <a href="https://tokentax.co/">tokentax.co</a>.
   </p>
   <p>
-  before that they were at the <a href="https://www.recurse.com/">recurse center</a> learning about <a href="https://en.wikipedia.org/wiki/Abstract_nonsense">abstract nonsense</a>, functional paradigms, and minimal web
+  before that they were at the <a href="https://www.recurse.com/">recurse center</a> learning about <a href="https://en.wikipedia.org/wiki/Abstract_nonsense">abstract nonsense</a>, functional programming, and minimal web
     frameworks.
   </p>
 </section>
