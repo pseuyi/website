0f0d13dcf801c8e6aa279eddfce29a41d536be0b
freda <pseuyi@gmail.com>
2024-01-13 12:37:52 -0500

    redesign

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="title">
+        <b>freda suyi ding</b>
       </header>
+
+      <details>
+        <summary className="summary">currently</summary>
+        <div className="info">
+          working on{" "}
+          <a
+            href="https://www.costarastrology.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ai-powered creative content generation
+          </a>{" "}
+          using functional programming paradigms.
+        </div>
+      </details>
+
+      <details>
+        <summary className="summary">about me</summary>
+        <div className="info">
+          i'm a computer programmer based in chinatown, nyc. <br />
+          i'm interested in writing elegant, safe, and functional software.
+          <ul>
+            <li>
+              <a
+                href="https://github.com/pseuyi"
+                target="_blank"
+                rel="noreferrer"
+              >
+                github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/pseuyi/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                linkedin
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.are.na/after-freda"
+                target="_blank"
+                rel="noreferrer"
+              >
+                are.na
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://docs.google.com/document/d/18GQSqlOH4n46326_tAB26Go9NKEBiMQGqiRomDJTP6w/edit?usp=sharing"
+                rel="noreferrer"
+              >
+                resume
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://special.fish/after_freda"
+                target="_blank"
+                rel="noreferrer"
+              >
+                𓆝 𓆟 𓆞
+              </a>
+            </li>
+            <li>
+              <a href="https://art.pseuyi.dev" rel="noreferrer">
+                art
+              </a>
+            </li>
+          </ul>
+        </div>
+      </details>
+
+      <details>
+        <summary className="summary">archive</summary>
+        <div className="info">
+          past iterations of this site
+          <ul>
+            <li>
+              <a
+                href="https://pseuyi.dev/2016/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2016
+              </a>
+            </li>
+          </ul>
+        </div>
+      </details>
     </div>
   );
 }
